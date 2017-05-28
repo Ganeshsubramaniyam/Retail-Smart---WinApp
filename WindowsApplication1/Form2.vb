8bd0e8c80ce3b879fd8995fe33c7950b238a4d67
Ganesh Subramaniyam <mrsjgk.guru@gmail.com>
2017-05-28 05:56:58 -0500

    Add files via upload

---
+++ WindowsApplication1/Form2.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form2
+
+    Sub display_data(ByVal dt1 As DataTable, ByVal date_str As String)
+        DataGridView1.DataSource = dt1
+        Label1.Text = "Sales On : " + date_str
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ WindowsApplication1/Form9.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form9
+    Private Sub Form9_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Normal
+        Dim pi As Integer
+        System.Threading.Thread.Sleep(10)
+        For pi = 0 To 100
+            ProgressBar1.Value = pi
+            System.Threading.Thread.Sleep(5)
+            If pi = 100 Then
+                Me.Dispose()
+            End If
+        Next
+    End Sub
+End Class
